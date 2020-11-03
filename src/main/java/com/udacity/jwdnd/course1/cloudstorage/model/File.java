52575b1415198de662d20d917ad94ef1f6e76ffb
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-11-03 15:13:34 +0100

    added to File model

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -3,10 +3,18 @@ package com.udacity.jwdnd.course1.cloudstorage.model;
 public class File {
     private Integer fileId;
     private String fileName;
+    private String contentType;
+    private String fileSize;
+    private Integer userId;
+    private byte[] fileData;
 
-    public File(Integer fileId, String fileName) {
+    public File(Integer fileId, String fileName, String contentType, String fileSize, Integer userId, byte[] fileData) {
         this.fileId = fileId;
         this.fileName = fileName;
+        this.contentType = contentType;
+        this.fileSize = fileSize;
+        this.userId = userId;
+        this.fileData = fileData;
     }
 
     public Integer getFileId() {
@@ -24,4 +32,37 @@ public class File {
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public byte[] getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(byte[] fileData) {
+        this.fileData = fileData;
+    }
 }
+
