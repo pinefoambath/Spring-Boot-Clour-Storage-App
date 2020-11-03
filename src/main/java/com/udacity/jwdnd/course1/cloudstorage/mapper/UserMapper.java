e69027468846be5b09d543b4ff1af099eb58d75f
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-11-03 14:43:44 +0100

    fixed user model bug

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -1,6 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
-import com.udacity.jwdnd.course1.cloudstorage.User;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
