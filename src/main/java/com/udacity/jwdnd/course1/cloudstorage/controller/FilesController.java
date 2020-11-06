9bee95e3b125b316aad011637495b07c5af19630
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-11-06 14:53:43 +0100

    ..

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FilesController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FilesController.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.model.FileForm;
 import org.springframework.stereotype.Controller;
 import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,7 @@ public class FilesController {
 
     private FileService fileService;
     private AuthenticationService authenticationService;
-
-    public FilesController(FileService fileService, AuthenticationService authenticationService) {
-        this.fileService = fileService;
-        this.authenticationService = authenticationService;
-    }
+    private FileForm fileForm;
 
     @PostMapping("/files")
     public String insertFile(AuthenticationService authenticationService, MultipartFile fileUpload) throws IOException {
@@ -24,7 +21,7 @@ public class FilesController {
         if (fileUpload.isEmpty()) {
             return "redirect:/result?error";
         }
-        fileService.addFile(fileUpload, user.loadUserByUsername());
+        fileService.addFile(fileForm);
         return "redirect:/result?success";
     }
 
