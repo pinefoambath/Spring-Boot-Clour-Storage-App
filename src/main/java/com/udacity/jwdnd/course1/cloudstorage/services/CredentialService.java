d88b2c73ec5e36faea2bb70989aa611ae52f7814
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-11-06 12:35:59 +0100

    ..

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.NoteMapper;
 import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.services.EncryptionService;
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,18 @@ import java.util.List;
 public class CredentialService {
 
     private CredentialMapper CredentialMapper;
+    private EncryptionService encryptionService;
 
-    public CredentialService(CredentialMapper credentialMapper) {
-        this.CredentialMapper = credentialMapper;
+    public CredentialService(CredentialMapper credentialMapper, EncryptionService encryptionService) {
+        CredentialMapper = credentialMapper;
+        this.encryptionService = encryptionService;
+    }
+
+    private Credentials encryptPassword(Credentials credential) {
+        String key = RandomStringUtils.random(16, true, true);
+        credential.setKey(key);
+        credential.setPassword(encryptionService.encryptValue(credential.getPassword(), key));
+        return credential;
     }
 
     public List<Credential> getCredentialsByUserId(int userId) throws Exception {
