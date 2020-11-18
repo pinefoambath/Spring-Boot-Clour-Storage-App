0fb36163cb20fafc5cc80111628b21ad8e055a49
Andrea Kreideweiss <akreideweiss@gmail.com>
2020-11-18 14:38:41 +0100

    added redirect from localhost8080 to login

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/signup", "/postLogin", "/css/**", "/js/**").permitAll()
-                .anyRequest().authenticated();
+//                .anyRequest().authenticated();
+                .antMatchers("/", "/home*", "/result*").authenticated();
         http.formLogin()
                 .loginPage("/login")
                 .permitAll()
